4aef2bae03145373a0a77c36764165bc427637e7
dsz79 <79846294+dsz79@users.noreply.github.com>
2021-11-10 10:57:58 +0100

    Update notify-card.js

--- notify-card.js
+++ notify-card.js
@@ -37,6 +37,7 @@ class NotifyCard extends HTMLElement {
   }
 
   send(){
+    let tts = this.content.querySelector("miCheck").checked;
     let msg = this.content.querySelector("paper-input").value;
     for (let t of this.targets) {
       let [domain, target = null] = t.split(".");
@@ -44,7 +45,7 @@ class NotifyCard extends HTMLElement {
         target = domain;
         domain = "notify";
       }
-      if(miCheckbox.checked) {
+      if(tts) {
         this.hass.callService(domain, target, {title: msg, message: "TTS", data: this.config.data});
       } else {
         this.hass.callService(domain, target, {message: msg, data: this.config.data});
